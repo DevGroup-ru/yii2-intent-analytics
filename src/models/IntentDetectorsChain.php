b6a41966df5512a5127207cfd6fd9fbc214cb130
Alexander <duvanskiy@skobeeff.ru>
2016-01-21 02:36:35 +0300

    orthodox commit

---
+++ src/models/IntentDetectorsChain.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace DevGroup\Analytics\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "intent_detectors_chain".
+ *
+ * @property integer $intent_id
+ * @property integer $intent_detectors_id
+ * @property integer $sort_order
+ *
+ * @property Intent $intent
+ * @property IntentDetectors $intentDetectors
+ */
+class IntentDetectorsChain extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%intent_detectors_chain}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['intent_id', 'intent_detectors_id'], 'required'],
+            [['intent_id', 'intent_detectors_id', 'sort_order'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'intent_id' => 'Intent ID',
+            'intent_detectors_id' => 'Intent Detectors ID',
+            'sort_order' => 'Sort Order',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIntent()
+    {
+        return $this->hasOne(Intent::className(), ['id' => 'intent_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIntentDetectors()
+    {
+        return $this->hasOne(IntentDetectors::className(), ['id' => 'intent_detectors_id']);
+    }
+}
---
+++ src/models/TrafficSources.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace DevGroup\Analytics\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "traffic_sources".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $class_name
+ * @property string $params
+ *
+ * @property IntentDetectors $id0
+ */
+class TrafficSources extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%traffic_sources}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['name', 'class_name', 'params'], 'string'],
+            [['id'], 'exist', 'skipOnError' => true, 'targetClass' => IntentDetectors::className(), 'targetAttribute' => ['id' => 'needs_traffic_sources_id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'class_name' => 'Class Name',
+            'params' => 'Params',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getId0()
+    {
+        return $this->hasOne(IntentDetectors::className(), ['needs_traffic_sources_id' => 'id']);
+    }
+}
